a1dcc91ec85a17669c504d4d3a93e56d6792bf90
Amogh Madan <amoghmadaan@gmail.com>
2023-10-15 17:48:34 +0530

    Apply theme control.

--- reception_register_webui/src/App.jsx
+++ reception_register_webui/src/App.jsx
@@ -1,17 +1,27 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Browser } from "./config";
 import { Home, Http404, Register } from "./pages";
+import { dark, light } from "./utils/themes";
 
 export default function App() {
+  const themes = { dark, light };
+  const mode = useSelector((state) => state.theme.value);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={Browser.ROOT} element={<Home />}></Route>
-        <Route path={Browser.REGISTER} element={<Register />}></Route>
-        <Route path={Browser.HTTP_404} element={<Http404 />}></Route>
-        <Route path={Browser.ASTERISK} element={<Http404 />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={themes[mode]}>
+      <CssBaseline>
+        <BrowserRouter>
+          <Routes>
+            <Route path={Browser.ROOT} element={<Home />}></Route>
+            <Route path={Browser.ENTRY} element={<Register />}></Route>
+            <Route path={Browser.HTTP_404} element={<Http404 />}></Route>
+            <Route path={Browser.ASTERISK} element={<Http404 />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
