6e4e299d0ab2933d23744c5c2e29fc4dbc3e83f6
Amogh Madan <amoghmadaan@gmail.com>
2023-10-15 17:47:29 +0530

    Get rid of CSS files, old structure and use new naming convention for hooks.

--- reception_register_webui/src/utils/themes.js
+++
@@ -1,7 +0,0 @@
-import { createTheme } from "@mui/material";
-
-const theme = createTheme({
-  palette: { primary: { main: "#007bff" }, secondary: { main: "#ff5722" } },
-});
-
-export default theme;
