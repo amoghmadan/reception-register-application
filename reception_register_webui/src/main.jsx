47d7bf2481488003a40cc9c7e8da41f4ad4ba20d
Amogh Madan <amoghmadaan@gmail.com>
2023-10-16 15:31:29 +0530

    Fix import order.

--- reception_register_webui/src/main.jsx
+++ reception_register_webui/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import store from "./state/redux";
