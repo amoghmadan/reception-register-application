643f3c9e8455bb43d153c0808417b5a61d8c9256
Amogh Madan <amoghmadaan@gmail.com>
2023-10-16 14:52:16 +0530

    Add .detail missing attribute

--- reception_register_webui/src/hooks/useUser.js
+++ reception_register_webui/src/hooks/useUser.js
@@ -7,12 +7,11 @@ import { loginUser } from "../state/redux/user";
 
 export default function useUser() {
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user.detail);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const getRetrieveUser = async () => {
-      console.log(user);
       if (user !== undefined) return;
       setLoading(true);
       const response = await api.get(API.V1.ACCOUNT_DETAIL);
