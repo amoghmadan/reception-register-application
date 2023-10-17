543f9eeddc3b7dbe40362fdb8d681b199f959e7d
Amogh Madan <amoghmadaan@gmail.com>
2023-10-17 19:58:15 +0530

    Fix table Headers.

--- reception_register_webui/src/components/EntryTable/index.jsx
+++ reception_register_webui/src/components/EntryTable/index.jsx
@@ -13,12 +13,17 @@ import { ListEntryContext } from "../../state/context/ListEntryProvider";
 export default function EntryTable() {
   const { entryList, loading } = useContext(ListEntryContext);
   const columns = [
-    "ID",
+    "Serial Number",
     "Name",
     "Phone",
     "Person to Meet",
     "Company",
     "Purpose",
+    "In",
+    "Sign",
+    "Out",
+    "Security Sign",
+    "Remarks",
   ];
 
   if (loading) return <>Loading...</>;
@@ -27,27 +32,50 @@ export default function EntryTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            {columns.map((column) => {
-              <TableCell align="right" key={column}>
+            {columns.map((column) => (
+              <TableCell align="center" key={column}>
                 {column}
-              </TableCell>;
-            })}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {entryList.map((row, index) => (
+          {entryList.length === 0 ? (
             <TableRow
-              key={row._id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell align="right">{index + 1}</TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.mobile}</TableCell>
-              <TableCell align="right">{row.personToMeet}</TableCell>
-              <TableCell align="right">{row.company}</TableCell>
-              <TableCell align="right">{row.purpose}</TableCell>
+              <TableCell align="center" colSpan={columns.length}>
+                No Content
+              </TableCell>
             </TableRow>
-          ))}
+          ) : (
+            entryList.map((row, index) => {
+              return (
+                <TableRow
+                  key={row._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center">{index + 1}</TableCell>
+                  <TableCell align="center">{row.name}</TableCell>
+                  <TableCell align="center">{row.mobile}</TableCell>
+                  <TableCell align="center">{row.personToMeet}</TableCell>
+                  <TableCell align="center">{row.company}</TableCell>
+                  <TableCell align="center">{row.purpose}</TableCell>
+                  <TableCell align="center">
+                    {new Date(row.in).toLocaleTimeString()}
+                  </TableCell>
+                  <TableCell align="center">{row.sign}</TableCell>
+                  <TableCell align="center">
+                    {row.out ? new Date(row.out).toLocaleTimeString() : "-"}
+                  </TableCell>
+                  <TableCell align="center">
+                    {row.securitySign || "-"}
+                  </TableCell>
+                  <TableCell align="center">{row.remarks || "-"}</TableCell>
+                </TableRow>
+              );
+            })
+          )}
         </TableBody>
       </Table>
     </TableContainer>
