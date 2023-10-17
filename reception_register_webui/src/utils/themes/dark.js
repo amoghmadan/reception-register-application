96a5de4b94408979d977d18b23b974acb0e43727
Amogh Madan <amoghmadaan@gmail.com>
2023-10-17 19:57:42 +0530

    Update theme colors.

--- reception_register_webui/src/utils/themes/dark.js
+++ reception_register_webui/src/utils/themes/dark.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@mui/material";
 
 const dark = createTheme({
-  palette: { primary: { main: "#ff5722" }, secondary: { main: "#007bff" } },
+  palette: { primary: { main: "#f4f4f5" }, secondary: { main: "#111827" } },
 });
 
 export default dark;
--- reception_register_webui/src/utils/themes/light.js
+++ reception_register_webui/src/utils/themes/light.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@mui/material";
 
 const light = createTheme({
-  palette: { primary: { main: "#007bff" }, secondary: { main: "#ff5722" } },
+  palette: { primary: { main: "#111827" }, secondary: { main: "#f4f4f5" } },
 });
 
 export default light;
