ae7ab5f3ad68879b8805d1c6eca70a02745eb8ad
Amogh Madan <amoghmadaan@gmail.com>
2023-10-25 20:29:30 +0530

    Add Media Folder and URL.

--- reception_register_api/src/settings.js
+++ reception_register_api/src/settings.js
@@ -1,7 +1,19 @@
+import fs from 'fs';
 import path from 'path';
 
 import 'dotenv/config';
 
-export const BASE_DIR = path.dirname(path.dirname(__filename));
+// Path to API directories.
+export const BASE_DIR = path.dirname(__filename);
+export const MEDIA_ROOT = path.join(path.dirname(BASE_DIR), 'media');
 
+// Create folders if they do not exists. Extend array entries.
+[MEDIA_ROOT].forEach((folder) => {
+  !fs.existsSync(folder) && fs.mkdirSync(folder);
+});
+
+// Database connectivity URI.
 export const MONGODB_URI = process.env.MONGODB_URI;
+
+// Media Root URL
+export const MEDIA_URL = '/media/';
